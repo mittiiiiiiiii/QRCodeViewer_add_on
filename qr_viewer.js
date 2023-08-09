2c8cd20c9e7a85f308e7eb2e29f86375cba77505
Mittiii <srp5628comust@gmail.com>
2023-08-10 01:33:19 +0900

    minimum

---
+++ qr_viewer.js
@@ -0,0 +1,19 @@
+//ポップアップ
+document.getElementById("url_qrcode_button").onclick=() => { 
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.runtime.sendMessage({ to_backscript: tabs[0].url });
+        make_qr_code (tabs[0].url); 
+        //console.log("URLをQR化できた");
+    });
+};
+
+function make_qr_code (text) {
+    var qrcode = new QRCode (document.getElementById("qrcode"), {
+        text: text,
+        width: 128,   
+        height: 128,
+        colorDark: "#000000",  
+        colorLight: "#ffffff",
+        correctLevel: QRCode.CorrectLevel.H,   
+    });
+}   
