4a3385ebafac850218398f58f14def6775e11c3c
Mittiii <srp5628comust@gmail.com>
2023-08-10 16:59:21 +0900

    ver2

--- qr_viewer.js
+++ qr_viewer.js
@@ -1,11 +1,9 @@
 //ポップアップ
-function proc(){ 
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.runtime.sendMessage({ to_backscript: tabs[0].url });
-        make_qr_code (tabs[0].url); 
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.runtime.sendMessage({ to_backscript: tabs[0].url });
+    make_qr_code (tabs[0].url); 
         //console.log("URLをQR化できた");
-    });
-};
+});
 
 function make_qr_code (text) {
     var qrcode = new QRCode (document.getElementById("qrcode"), {
